6d0b3de01df9e420f4f062e3a6963d558542124b
halagenie1 <135343192+halagenie1@users.noreply.github.com>
2023-06-03 19:21:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,4 @@
-# import streamlit as st
-# import pandas as pd
-# st.title("A Simple Streamlit Web App")
-# name = st.text_input("Enter your name", "")
-# st.write(f"Hello {name}!")
-# x = st.slider("Select an integer x", 0, 10, 1)
-# y = st.slider("Select an integer y", 0, 10, 1)
-# df = pd.DataFrame({"x": [x], "y": [y] , "x + y": [x + y]}, index = ["addition row"])
-# st.write(df)
-#######################################
-# from flask import Flask
-# import streamlit as st
-# m=""
-# app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#      m="ffffffffffffffffff"
-# @app.route('/input')
-# def index1():
-#      m="ffffffffff22222ffffffff"
-
-
-###########################################
 import streamlit as st
 
 
